e8f72074a43f0acfc1fe9f572a2d44ee182d7a93
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-04-25 14:40:32 -0600

    add code of fragments

--- src/app/src/main/java/com/example/android/tours_mobile/ui/favorites/FavoritesViewModel.kt
+++ src/app/src/main/java/com/example/android/tours_mobile/ui/favorites/FavoritesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.android.tours_mobile.ui.notifications
+package com.example.android.tours_mobile.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notification Fragment"
+        value = "This is favorites Fragment"
     }
     val text: LiveData<String> = _text
 }
--- src/app/src/main/java/com/example/android/tours_mobile/ui/reservations/ReservationsViewModel.kt
+++ src/app/src/main/java/com/example/android/tours_mobile/ui/reservations/ReservationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.android.tours_mobile.ui.home
+package com.example.android.tours_mobile.ui.reservations
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ReservationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is reservations Fragment"
     }
     val text: LiveData<String> = _text
 }
