3c02891926b4ce611fe92bac0939f6bae5986038
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-05-01 19:42:44 -0600

    Add Code in Kotlin FragmenExplorer, RecyclerAdapter

--- src/app/src/main/java/com/example/android/tours_mobile/ui/explore/ExploreFragment.kt
+++ src/app/src/main/java/com/example/android/tours_mobile/ui/explore/ExploreFragment.kt
@@ -4,28 +4,74 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android.tours_mobile.R
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
 
 class ExploreFragment : Fragment() {
 
-    private lateinit var exploreViewModel: ExploreViewModel
+    private var adapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    private var layoutManager : RecyclerView.LayoutManager? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        exploreViewModel =
-                ViewModelProvider(this).get(ExploreViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_explore, container, false)
-        val textView: TextView = root.findViewById(R.id.text_explore)
-        exploreViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        val view = inflater.inflate(R.layout.fragment_explore, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+
+        val photos :  MutableList<String> = mutableListOf();
+        val places :  MutableList<String> = mutableListOf();
+        val names :  MutableList<String> = mutableListOf();
+        val prices :  MutableList<Float> = mutableListOf();
+        val ratings :  MutableList<Float> = mutableListOf();
+        val reviews :  MutableList<Int> = mutableListOf();
+
+        try {
+            val data = JSONObject(getJsonFromAssets("tours.json"))
+            val tours: JSONArray = data.getJSONArray("tours")
+            for (i in 0 until tours.length()) {
+                val tour = tours.getJSONObject(i)
+                val images: JSONArray = tour.getJSONArray("images")
+                photos.add(images.getJSONObject(0).getString("photo")) // Get Main Photo
+                names.add(tour.getString("name"))
+                places.add(tour.getString("completePlace"))
+                reviews.add(tour.getInt("reviews"));
+                prices.add(tour.getString("price").toFloat())
+                ratings.add(tour.getString("rating").toFloat())
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        }
+
+        adapter = RecyclerAdapter(photos, places, names, ratings, prices, reviews)
+        val layoutManager : LinearLayoutManager = LinearLayoutManager(context)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
+
+        return view
+    }
+
+    private fun getJsonFromAssets(fileName: String): String {
+        return try {
+            val file: InputStream = requireActivity().assets.open(fileName)
+            val size = file.available()
+            val buffer = ByteArray(size)
+            file.read(buffer)
+            file.close()
+            String(buffer, StandardCharsets.UTF_8)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            "{}"
+        }
     }
 }
