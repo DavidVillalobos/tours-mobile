8e3672121b4b5329579d587cddf38dff18663208
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-06-10 05:29:14 -0600

    fix: update TourDTO

--- src/app/src/main/java/com/example/android/tours_mobile/services/dto/ImageTourDTO.kt
+++ src/app/src/main/java/com/example/android/tours_mobile/services/dto/ImageTourDTO.kt
@@ -3,7 +3,7 @@ package com.example.android.tours_mobile.services.dto
 data class ImageTourDTO(
         val id: Int,
         val tour : TourDTO,
-        val photo : ByteArray,
+        val photo : String,
         val photoBase64 : String,
         val mainPhoto : Int
 )
--- src/app/src/main/java/com/example/android/tours_mobile/services/dto/TourDTO.kt
+++ src/app/src/main/java/com/example/android/tours_mobile/services/dto/TourDTO.kt
@@ -1,9 +1,9 @@
 package com.example.android.tours_mobile.services.dto
 
-data class TourDTO(val id: Int, val city: CityDTO, val likes: List<LikeTourDTO>,
-                   val images: List<ImageTourDTO>, val comments: List<CommentTourDTO>,
+data class TourDTO(val id: Int, val city: CityDTO, val likes: List<LikeTourDTO>?,
+                   val images: List<ImageTourDTO>?, val comments: List<CommentTourDTO>?,
                    val name: String, val category: String, val description: String,
-                   val date: String, val quota: Int, val reviews: Int, val time : String,
+                   val date: String, val quota: Int, val reviews: Int, val duration : String,
                    val price: Float, val rating : Float, val includes: String,
-                   val notIncludes: String, val liked : Boolean, val getCompletePlace : String
+                   val notIncludes: String, val liked : Boolean, val completePlace : String
 )
